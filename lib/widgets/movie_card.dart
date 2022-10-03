7b0ad7a150a8ee2b555cc9f1795532f9eeadf29f
oreste <muhirwa.g.oreste@gmail.com>
2022-10-04 00:13:54 +0200

    Extracting the details widget for better coding practice

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -16,64 +16,64 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            elevation: 3,
-            child: SizedBox(
-              height: 100,
-              child: Row(
-                children: [
-                  Image.network(
-                    'https://image.tmdb.org/t/p/w500$posterPath',
-                  ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.only(top: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ///For some reason, some titles are null, and must be treated to not cause the app to crush
-                          Text(
-                            title,
-                            style: const TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                          ),
-
-                          Text(
-                            getFrenchDate(
-                              DateTime.parse(releaseDate),
-                            ),
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                          const Spacer(),
-                          Text(
-                            ///Tesing if data exist before usage
-                            overview,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 6,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 3,
+        child: SizedBox(
+          height: 100,
+          child: Row(
+            children: [
+              Image.network(
+                'https://image.tmdb.org/t/p/w500$posterPath',
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: _moviesInfo(
+                      title: title,
+                      releaseDate: releaseDate,
+                      overview: overview),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// The column on the right of the image
+  Widget _moviesInfo({
+    required String title,
+    required String releaseDate,
+    required String overview,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ///The title should be displayed on one line, otherwise, the ellipsis (...) are shown
+        Text(
+          title,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+        Text(
+          getFrenchDate(
+            DateTime.parse(releaseDate),
+          ),
+          style: const TextStyle(color: Colors.grey),
+        ),
+        const Spacer(),
+
+        ///The overview is only show on 2 lines, and the rest is replaced by (...)
+        Text(
+          overview,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          style: const TextStyle(
+            fontSize: 16,
           ),
         ),
       ],
